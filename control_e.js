ea040ee9e097ac9685e5a3eb7d8d2f80b3591cd7
ledanhduc <20161182@student.hcmute.edu.vn>
2025-06-13 00:44:55 +0700

    log his web butt

--- control_e.js
+++ control_e.js
@@ -57,7 +57,7 @@ function toggleLamp(toggleElem, stateElem, path) {
 function handleIdDeviceUpdate(value) {
 
   updateLinkHrefs(pageLinks_e, currentUrl, idDevice, value);
-  
+  const timestamp = new Date().toLocaleString().replace(/[/]/g, '-');
 
   let lamps = [
   {toggle: document.getElementById('lamp_1_toggle'), state: document.getElementById('lamp_1_state'), path: `${value}/lamp_1_state`},
@@ -66,6 +66,7 @@ function handleIdDeviceUpdate(value) {
   lamps.forEach(function(lamp) {
   lamp.toggle.addEventListener('click', function() {
     toggleLamp(lamp.toggle, lamp.state, lamp.path);
+    set(ref(database, `${value}/WebButt/${encodedEmail}/${timestamp}`), lamp.state.innerHTML);
   });
   });
 
@@ -76,30 +77,26 @@ function handleIdDeviceUpdate(value) {
 
   lamps_fb.forEach(function(lamp_fb) {
     onValue(ref(database, lamp_fb.path), function(snapshot) {
-      const timestamp = new Date().toLocaleString().replace(/[/]/g, '-');
       let state = snapshot.val();
       if (state) {
         lamp_fb.toggle.parentNode.classList.add('active');
         lamp_fb.state.innerHTML = "ON";
         lamp_fb.state.style.color = "rgb(57,198,92)";
-        // set(ref(database, `${value}/1/web_butt/${timestamp}`), true);
       } else {
         lamp_fb.toggle.parentNode.classList.remove('active');
         lamp_fb.state.innerHTML = "OFF";
         lamp_fb.state.style.color = "rgb(227, 4, 90)";
-        // set(ref(database, `${value}/1/web_butt/${timestamp}`), false);
-        // set(ref(database, `${value}/st_timer`), false); 
       }
     });
   });
 
   var currentSecond;
-  var lastOnlineTime = 0;
   var ms;
   const st_cir = document.getElementById('st_cir');
   let onlesp;
-
+  
   function sendCurrentSecond() {
+    var lastOnlineTime = 0;
     currentSecond = new Date().getSeconds(); // Lấy giây hiện tại
     ms = currentSecond; // Đổi sang chỉ tính bằng giây
 
@@ -129,44 +126,6 @@ function handleIdDeviceUpdate(value) {
   // Cập nhật mỗi giây
   setInterval(sendCurrentSecond, 1 * 1000);
 
-
-  const butt_timer = document.getElementById('butt_timer');
-  butt_timer.addEventListener('click', function() {
-    const timeInput = document.getElementById("timeInput");
-    const selectedTime = timeInput.value;
-
-    const [hour, minute] = selectedTime.split(":");
-    set(ref(database, `${value}/h_timer`), parseInt(hour, 10)); 
-    set(ref(database, `${value}/m_timer`), parseInt(minute, 10)); 
-    set(ref(database, `${value}/st_timer`), true); 
-    alert("Timer is on");
-  });
-
-
-  const h_timerRef = ref(database, `${value}/h_timer`);
-  const m_timerRef = ref(database, `${value}/m_timer`);
-  const st_timerRef = ref(database, `${value}/st_timer`);
-  function checkTimer(){
-    Promise.all([
-      get(h_timerRef),
-      get(m_timerRef),
-      get(st_timerRef)
-    ]).then(([h_snapshot, m_snapshot, st_snapshot]) => {
-      const h_timer = h_snapshot.val();
-      const m_timer = m_snapshot.val();
-      const st_timer = st_snapshot.val();
-
-      if (st_timer == true) {
-        document.getElementById('st_timer').style.color = "rgb(29 154 60)";
-        document.getElementById('st_timer').style.fontWeight = "800";
-        document.getElementById('st_timer').innerText = `device will off at ${h_timer} : ${m_timer}`;
-      }else{
-        document.getElementById('st_timer').removeAttribute('style');
-        document.getElementById('st_timer').innerText = "Timer";
-      }
-    });
-  }
-  setInterval(checkTimer, 10 * 1000);
 }
 
 
